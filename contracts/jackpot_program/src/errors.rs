afd9737b0e34ace1a278a993328d457f1d68d9fa
JACKPOT <dev@playjackpot.fun>
2025-05-16 12:42:05 -0500

    Update errors.rs

--- contracts/jackpot_program/src/errors.rs
+++ contracts/jackpot_program/src/errors.rs
@@ -5,7 +5,17 @@ pub enum ErrorCode {
     #[msg("Invalid geolocation coordinates")]
     InvalidLocation,
     #[msg("Player has not completed required hides")]
-    InvalidHideSequence,
+    InvalidSeekSequence,
     #[msg("Insufficient $SEEK balance")]
     InsufficientSeekBalance,
+    #[msg("No NFT provided for hint")]
+    NoNFTProvided,
+    #[msg("KYC not verified for BTC hide")]
+    KYCNotVerified,
+    #[msg("Unauthorized access")]
+    Unauthorized,
+    #[msg("Player ineligible for airdrop")]
+    IneligibleForAirdrop,
+    #[msg("Airdrop limit reached")]
+    AirdropLimitReached,
 }
