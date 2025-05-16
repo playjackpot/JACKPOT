4f345f24df9bc4185ca77158d2d6e7f4361bc437
JACKPOT <dev@playjackpot.fun>
2025-05-16 12:17:37 -0500

    Update errors.rs

--- contracts/btc_rewards_program/src/errors.rs
+++ contracts/btc_rewards_program/src/errors.rs
@@ -9,3 +9,18 @@ pub enum ErrorCode {
     #[msg("Player not in 101–200")]
     NotSilverPlayer,
 }
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Insufficient BTC balance")]
+    InsufficientBTCBalance,
+    #[msg("Player not in top 100")]
+    NotGoldPlayer,
+    #[msg("Player not in 101–200")]
+    NotSilverPlayer,
+    #[msg("Annual BTC hide already created for this year")]
+    AnnualHideAlreadyCreated,
+    #[msg("KYC not verified")]
+    KYCNotVerified,
+}
