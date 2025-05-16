3847c7455874824bb507ce9f5e15d34730e29714
JACKPOT <dev@playjackpot.fun>
2025-05-16 12:18:16 -0500

    Update state.rs

--- contracts/btc_rewards_program/src/state.rs
+++ contracts/btc_rewards_program/src/state.rs
@@ -4,7 +4,9 @@ use anchor_lang::prelude::*;
 pub struct BTCWalletState {
     pub btc_rewards_wallet: Pubkey,  // WBTC account for 1 BTC hides
     pub btc_drop_wallet: Pubkey,    // WBTC account for excess airdrops
-    pub sol_profit_wallet: Pubkey,  // Reference to SOL Profit Wallet
+    pub sol_profit_wallet: Pubkay,  // Reference to SOL Profit Wallet
     pub fees_collected: u64,        // Total fees collected
     pub year: u32,                  // Current game year
+    pub annual_hide_created: bool,  // Tracks if annual BTC hide is created for the year
+    pub annual_hide: Option<Pubkey>, // Reference to the hide account in jackpot_program
 }
